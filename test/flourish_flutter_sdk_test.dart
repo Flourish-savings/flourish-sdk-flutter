5d2365cea40bcc29cfb309e8f9d3161cd6c67db9
Allan Goncalves <allangoncalves@outlook.com>
2020-09-15 19:27:59 -0300

    add test cases

--- test/flourish_flutter_sdk_test.dart
+++ test/flourish_flutter_sdk_test.dart
@@ -1,23 +1,38 @@
-import 'package:flutter/services.dart';
+import 'package:flourish_flutter_sdk/environment_enum.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flourish_flutter_sdk/flourish_flutter_sdk.dart';
+import 'package:flourish_flutter_sdk/flourish.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('flourish_flutter_sdk');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
+  Flourish flourish;
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    flourish = Flourish.initialize(Environment.development);
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    flourish = null;
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlourishFlutterSdk.platformVersion, '42');
+  test('authenticate the client', () async {
+    expect(flourish.authenticate('id', 'random_string'), 'key');
+  });
+
+  test('authenticate the client and open the dasboard', () async {
+    flourish.authenticateAndOpenDashboard('id', 'random_string');
+    expect(flourish.webviewContainer(), isNotNull);
+  });
+
+  test('certify that the webview is not initialized before auth', () async {
+    expect(flourish.webviewContainer(), isNull);
+  });
+
+  test('assert that only one instance can be created', () async {
+    Flourish instance2 = Flourish.initialize(Environment.development);
+    expect(identical(instance2, flourish), true);
+  });
+  test('assert that the instance is changing the environment attribute',
+      () async {
+    Flourish instance2 = Flourish.initialize(Environment.development);
+    expect(instance2.environment, Environment.development);
   });
 }
