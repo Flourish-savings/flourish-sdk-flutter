06a3f99cebad755269c0698f9cffe8d88acc6bed
Rafael Kerr <rafael@flourishfi.com>
2024-04-15 16:59:13 -0300

    changes

--- lib/web_view/webview_container.dart
+++ lib/web_view/webview_container.dart
@@ -1,32 +1,41 @@
 import 'dart:convert';
 
+import 'package:clipboard/clipboard.dart';
 import 'package:flourish_flutter_sdk/config/endpoint.dart';
 import 'package:flourish_flutter_sdk/config/environment_enum.dart';
 import 'package:flourish_flutter_sdk/events/event.dart';
 import 'package:flourish_flutter_sdk/events/event_manager.dart';
 import 'package:flourish_flutter_sdk/flourish.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flourish_flutter_sdk/web_view/error_view.dart';
 import 'package:flutter/material.dart';
-
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../config/language.dart';
 import '../events/types/web_view_loaded_event.dart';
 
 class WebviewContainer extends StatefulWidget {
+  final Environment environment;
+  final String apiToken;
+  final Language language;
+  final EventManager eventManager;
+  final Endpoint endpoint;
+  final Flourish flourish;
+  final String? version;
+  final String? trackingId;
+
   WebviewContainer({
     Key? key,
     required this.environment,
     required this.apiToken,
+    required this.language,
     required this.eventManager,
     required this.endpoint,
     required this.flourish,
+    this.version,
+    this.trackingId,
   }) : super(key: key);
 
   final WebviewContainerState _wcs = new WebviewContainerState();
-
-  final Environment environment;
-  final String apiToken;
-  final EventManager eventManager;
-  final Endpoint endpoint;
-  final Flourish flourish;
 
   void loadUrl(String url) {
     _wcs.loadUrl(url);
@@ -47,15 +56,35 @@ class WebviewContainerState extends State<WebviewContainer> {
     this._controller.loadUrl(url);
   }
 
-  void config( Flourish flourish) {
+  void config(Flourish flourish) {
     this.flourish = flourish;
   }
 
   @override
   Widget build(BuildContext context) {
-    String url = widget.endpoint.getFrontend();
-    String fullUrl = "$url&token=${widget.apiToken}";
-    //debugPrint("Full URL $fullUrl");
+    String url = "";
+    String fullUrl = "";
+
+    if (widget.version != null && widget.version != "") {
+      url = widget.version == "V2"
+          ? widget.endpoint.getFrontendV2()
+          : widget.endpoint.getFrontendV3();
+
+      fullUrl = widget.version == "V2"
+          ? "${url}/${widget.language.code()}?token=${widget.apiToken}"
+          : "${url}?${widget.language.code()}&token=${widget.apiToken}";
+
+    } else {
+      url = widget.endpoint.getFrontendV3();
+      fullUrl = "${url}?${widget.language.code()}&token=${widget.apiToken}";
+    }
+
+    if(widget.trackingId != null) {
+      fullUrl = "${fullUrl}&ga_tracking=${widget.trackingId}";
+    }
+
+    print(fullUrl);
+
     return Container(
       color: Theme.of(context).primaryColor,
       child: SafeArea(
@@ -67,8 +96,11 @@ class WebviewContainerState extends State<WebviewContainer> {
             print(error.description);
             print(error.domain);
           },
-          navigationDelegate: (action) {
-            print(action.url);
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.endsWith('.pdf')) {
+              _launchURL(request.url);
+              return NavigationDecision.prevent;
+            }
             return NavigationDecision.navigate;
           },
           javascriptMode: JavascriptMode.unrestricted,
@@ -78,6 +110,14 @@ class WebviewContainerState extends State<WebviewContainer> {
                 onMessageReceived: (JavascriptMessage message) {
                   Map<String, dynamic> json = jsonDecode(message.message);
                   final eventName = json['eventName'];
+                  if (eventName == "RetryLogin") {
+                    openErrorScreen();
+                    return;
+                  }
+                  if (eventName == "REFERRAL_COPY") {
+                    FlutterClipboard.copy(json['data']['referralCode']);
+                    return;
+                  }
                   Event event = Event.fromJson(json);
                   this._notify(event);
                 })
@@ -92,7 +132,20 @@ class WebviewContainerState extends State<WebviewContainer> {
     );
   }
 
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    await launchUrl(uri);
+  }
+
   void _notify(Event event) {
     widget.eventManager.notify(event);
+  }
+
+  void openErrorScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ErrorView(flourish: this.flourish)),
+    );
   }
 }
